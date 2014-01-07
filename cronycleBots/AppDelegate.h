10a69de09fa3bf70e1a1236b6c311d22d05c822c
Alex Di Mango <alex.dimango@gmail.com>
2014-01-07 11:46:55 +0000

    Initial Commit

---
+++ cronycleBots/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cronycleBots
+//
+//  Created by Alex Di Mango on 07/01/2014.
+//  Copyright (c) 2014 Alex Di Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ cronycleBotsTests/cronycleBotsTests.m
@@ -0,0 +1,34 @@
+//
+//  cronycleBotsTests.m
+//  cronycleBotsTests
+//
+//  Created by Alex Di Mango on 07/01/2014.
+//  Copyright (c) 2014 Alex Di Mango. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface cronycleBotsTests : XCTestCase
+
+@end
+
+@implementation cronycleBotsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
